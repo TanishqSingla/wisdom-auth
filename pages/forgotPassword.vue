1a5133ac81c762bc83bcd71c32bbcce88e93e1de
TanishqSingla <tanishqsingla00@gmail.com>
2022-11-22 23:37:36 +0530

    Add forgot password page

--- pages/forgotPassword.vue
+++ pages/forgotPassword.vue
@@ -1,13 +1,13 @@
 <script>
-import { inputErrors, isEmail } from '~~/util/utils';
+import { inputErrors, isEmail } from "~~/util/utils";
 
 export default {
   data() {
     return {
       emailError: "",
       email: "",
-      visible: false
-    }
+      visible: false,
+    };
   },
   methods: {
     validateEmail(e) {
@@ -16,18 +16,25 @@ export default {
         this.emailError = inputErrors.validEmail;
         return;
       }
-      this.emailError = ""
+      this.emailError = "";
     },
-    async submitForm() {
-      const { data } = await useFetch('http://localhost:4000/api/user/recoverPassword', {
+    submitForm() {
+      fetch("http://localhost:4000/api/user/recoverPassword", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: this.email }),
-      });
-      this.visible = true;
-    }
-  }
-}
+      }).then(async (res) => {
+        const data = await res.json();
+        if(data.message) {
+          this.visible = true;
+          return;
+        } else {
+          this.emailError = data.error;
+        }
+      }).catch((e) => console.log(e));
+    },
+  },
+};
 </script>
 
 <template>
@@ -37,27 +44,34 @@ export default {
         <div class="form-container">
           <div class="form-caption">
             <h1 class="caption-title">Forgot Password</h1>
-            <p>We'll send you a reset password link to your registered email address</p>
+            <p>
+              We'll send you a reset password link to your registered email
+              address
+            </p>
           </div>
           <form class="form" @submit.prevent="submitForm">
-            <input name="emailOrPhone" placeholder="Registered Email" v-model="email" v-on:blur="validateEmail"
+            <input name="emailOrPhone" placeholder="Registered Email" v-model="email" v-on:blur="validateEmail" v-on:input="validateEmail"
               :class="emailError ? 'border-danger' : ''" />
             <span class="text-danger">{{ emailError }}</span>
-            <button class="btn-primary" type="submit" :disabled="emailError.length !== 0">Submit</button>
-            <NuxtLink to="/"><button class="btn-secondary w-full">Return to Sign In</button></NuxtLink>
+            <button class="btn-primary" type="submit" :disabled="emailError.length !== 0">
+              Submit
+            </button>
+            <NuxtLink to="/"><button class="btn-secondary w-full">
+                Return to Sign In
+              </button></NuxtLink>
           </form>
         </div>
       </div>
       <Teleport to="body">
-        <Modal v-show="visible" v-on:closeModal="() => visible = false">
-          <template #children>
-            <div class="modalContainer">
-              <h1>Reset Password</h1>
-              <p>
-                Thank you! We have sent you a link to reset your password. Please
-                check your email
-              </p>
-            </div>
+        <Modal v-show="visible" v-on:closeModal="() => (visible = false)">
+          <template #title>
+            <h1 class="modal-title">Reset Password</h1>
+          </template>
+          <template #content>
+            <p>
+              Thank you! We have sent you a link to reset your password. Please
+              check your email.
+            </p>
           </template>
         </Modal>
       </Teleport>
