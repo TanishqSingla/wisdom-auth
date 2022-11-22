ac27ef104547843c393fcd03defea1e2338e0703
TanishqSingla <tanishqsingla00@gmail.com>
2022-11-22 22:55:02 +0530

    Show modal on success

--- pages/forgotPassword.vue
+++ pages/forgotPassword.vue
@@ -6,6 +6,7 @@ export default {
     return {
       emailError: "",
       email: "",
+      visible: false
     }
   },
   methods: {
@@ -18,7 +19,12 @@ export default {
       this.emailError = ""
     },
     async submitForm() {
-      const {data} = await useFetch('/api/user/forgotPassword');
+      const { data } = await useFetch('http://localhost:4000/api/user/recoverPassword', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email: this.email }),
+      });
+      this.visible = true;
     }
   }
 }
@@ -35,13 +41,26 @@ export default {
           </div>
           <form class="form" @submit.prevent="submitForm">
             <input name="emailOrPhone" placeholder="Registered Email" v-model="email" v-on:blur="validateEmail"
-              :class="emailError?'border-danger':''" />
+              :class="emailError ? 'border-danger' : ''" />
             <span class="text-danger">{{ emailError }}</span>
-            <button class="btn-primary" type="submit" :disabled="emailError.length!==0">Submit</button>
+            <button class="btn-primary" type="submit" :disabled="emailError.length !== 0">Submit</button>
             <NuxtLink to="/"><button class="btn-secondary w-full">Return to Sign In</button></NuxtLink>
           </form>
         </div>
       </div>
+      <Teleport to="body">
+        <Modal v-show="visible" v-on:closeModal="() => visible = false">
+          <template #children>
+            <div class="modalContainer">
+              <h1>Reset Password</h1>
+              <p>
+                Thank you! We have sent you a link to reset your password. Please
+                check your email
+              </p>
+            </div>
+          </template>
+        </Modal>
+      </Teleport>
     </template>
   </Layout>
 </template>
