8175d721f648745e116f199080a0dd61b3dac128
TanishqSingla <tanishqsingla00@gmail.com>
2022-11-23 02:07:00 +0530

    Add password input

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,3 +1,14 @@
+
+<script>
+export default {
+  data() {
+    return {
+      showPassword: false
+    }
+  },
+}
+</script>
+
 <template>
   <Layout>
     <template #children>
@@ -5,26 +16,40 @@
         <div class="form-container">
           <div class="form-caption">
             <h1 class="caption-title">Create an account</h1>
-            <p>Already have an account? <span class="text-accent font-bold">Sign In</span></p>
+            <p>
+              Already have an account?
+              <span class="text-accent font-bold">Sign In</span>
+            </p>
           </div>
           <form class="form" @submit.prevent="">
             <input placeholder="First Name" />
             <input placeholder="Last Name" />
             <input placeholder="Email Address" />
-            <div>
-              <select>
-                <option value="+91">
-                </option>
-              </select>
+            <div class="flex gap-2">
+              <label class="border p-2 rounded-md w-24">
+                <select>
+                  <option value="+91">+91</option>
+                </select>
+              </label>
+              <input placeholder="Mobile Number" />
             </div>
+            <label>
+              <div class="relative">
+                <input placeholder="Password" :type="showPassword ? 'text' : 'password'" />
+                <img :src="showPassword ? '_nuxt/assets/svgs/eye-closed.svg' : '_nuxt/assets/svgs/eye.svg'"
+                  v-on:click="() => showPassword = !showPassword" class="absolute right-4 bottom-4" />
+              </div>
+              <span class="text-xs mt-2">Password must be at least 8 characters</span>
+            </label>
+            <p class="text-xs">
+              By clicking Sign Up you are indicating that you have read and
+              acknowledged the <span class="text-accent">Terms of Service</span> and <span class="text-accent">Privacy
+                Notice</span>
+            </p>
+            <button class="btn-primary">Sign Up</button>
           </form>
         </div>
       </div>
     </template>
   </Layout>
 </template>
-
-<script>
-export default {
-}
-</script>
