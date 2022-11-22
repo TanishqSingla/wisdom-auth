f8eb97fb17b28064413a341e404ca319f2977995
TanishqSingla <tanishqsingla00@gmail.com>
2022-11-23 01:27:54 +0530

    Using image tags instead of svg in components

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,15 +1,30 @@
 <template>
   <Layout>
     <template #children>
-      <div></div>
+      <div class="form-section">
+        <div class="form-container">
+          <div class="form-caption">
+            <h1 class="caption-title">Create an account</h1>
+            <p>Already have an account? <span class="text-accent font-bold">Sign In</span></p>
+          </div>
+          <form class="form" @submit.prevent="">
+            <input placeholder="First Name" />
+            <input placeholder="Last Name" />
+            <input placeholder="Email Address" />
+            <div>
+              <select>
+                <option value="+91">
+                </option>
+              </select>
+            </div>
+          </form>
+        </div>
+      </div>
     </template>
   </Layout>
 </template>
 
 <script>
 export default {
-  name: 'SignUp',
-  components: {
-  }
 }
 </script>
